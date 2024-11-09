3b802e12f2c26c5856e4f09bb22a6581bc406ad0
kuroineko23 <73019701+kuroineko23@users.noreply.github.com>
2024-11-09 22:04:54 +0700

    help

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 app.post('/sendcode', authMiddleware, (req, res) => {
-    io.to(req.body.roomId).emit("message", req.body.code)
+    io.to(req.body.roomId).emit("message", req.body)
     res.sendStatus(200)
 })
 
