3484193b3492956a08889538e197ec8fa2afa22b
kuroineko23 <73019701+kuroineko23@users.noreply.github.com>
2024-11-09 21:32:44 +0700

    get socket.io js from cdn instead

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server({
+    serveClient: false
+  });
 const cors = require('cors');
 const { authMiddleware } = require('./auth');
 
